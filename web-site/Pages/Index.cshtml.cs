d1e650594910aa26a755bdd7c20ef7b8b46c78db
David Walker <david.walker@etcconnect.com>
2024-02-07 04:02:59 -0500

    Iterate

--- web-site/Pages/Index.cshtml.cs
+++ web-site/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
 
 namespace web_site.Pages;
 
@@ -12,8 +13,35 @@ public class IndexModel : PageModel
         _logger = logger;
     }
 
+    public List<(string Name, string Id)> Users { get; set; } = new ();
+
     public void OnGet()
     {
 
+        // Establish a connection to the database using the custom SQL Server connection string
+        string connectionString = "Server=ms-sql-server,1433;Database=YourDatabaseName;TrustServerCertificate=True;User Id=sa;Password=YourStrongPassword99@;";
+        using (var connection = new SqlConnection(connectionString))
+        {
+            // Open the connection
+            connection.Open();
+
+            // Create a SQL command to query the database
+            string query = "SELECT Name,Id FROM YourTableName";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            // Execute the command and retrieve the data
+            SqlDataReader reader = command.ExecuteReader();
+
+            // Read the data and populate the list of users
+            while (reader.Read())
+            {
+                Users.Add((reader.GetString(1), reader.GetString(0)));
+            }
+
+            // Close the reader and the connection
+            reader.Close();
+            connection.Close();
+
+        }
     }
 }
