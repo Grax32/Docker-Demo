1c25f01c983971c95debad17157000c5889d1af1
David Walker <david.walker@etcconnect.com>
2024-02-08 20:40:31 -0500

    Iterate

--- ms-sql-server/build.cmd
+++
@@ -1,4 +0,0 @@
-
-@REM Remove the existing image and build a new one
-docker rmi custom-sql-server --force
-docker build -t custom-sql-server . --no-cache
--- ms-sql-server/run.cmd
+++
@@ -1,7 +0,0 @@
-@REM stop if running
-docker stop custom-sql-server
-
-@REM remove if exists
-docker rm custom-sql-server
-
-docker run -d --name custom-sql-server -p1492:1433 custom-sql-server 
